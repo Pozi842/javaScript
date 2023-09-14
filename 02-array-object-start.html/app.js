fea941c6e0d1f5bf0c9953a8c216fd4ac11f97f1
Pozi842 <84508246+Pozi842@users.noreply.github.com>
2023-09-14 18:04:45 +0300

    fix bug

--- 02-array-object-start.html/app.js
+++ 02-array-object-start.html/app.js
@@ -61,10 +61,16 @@ createBtn.onclick = function () {
 
 listElement.onclick = function (event) {
     if (event.target.dataset.index) {
-        // console.log(event.target.dataset.index)
         const index = parseInt(event.target.dataset.index)
         const type = event.target.dataset.type
 
+        if (type === 'toggle') {
+            notes[index].completed=!notes[index].completed
+        }
+        else if (type === 'remove') {
+            notes.splice(index, 1)
+        }
+        render()            
     }
 }
 
@@ -75,8 +81,8 @@ function getNoteTemplate(note, index){
             >
             <span class="${note.completed ? 'text-decoration-line-through' : ''}">${note.title}</span>
             <span>
-            <span class="btn btn-small btn-${note.completed ? 'warning' : 'success' }" data-index"${index}">&check;</span>
-            <span class="btn btn-small btn-danger">&times;</span>
+            <span class="btn btn-small btn-${note.completed ? 'warning' : 'success' }" data-index="${index}" data-type="toggle">&check;</span>
+            <span class="btn btn-small btn-danger" data-index="${index}" data-type="remove">&times;</span>
             </span>
             </li>`
 }
